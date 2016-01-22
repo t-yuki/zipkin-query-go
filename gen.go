8e0c86f1aa728c6011518aef498266ae0b0685a1
Yukinari Toyota <xxseyxx@gmail.com>
2016-01-23 02:19:54 +0900

    initial import

---
+++ gen.go
@@ -0,0 +1,4 @@
+//go:generate swagger generate server -f https://raw.githubusercontent.com/openzipkin/zipkin-api/master/zipkin-api.yaml
+//go:generate swagger generate client -f https://raw.githubusercontent.com/openzipkin/zipkin-api/master/zipkin-api.yaml
+
+package zipkin_server_go
